08a2713ea30b1e389b6aa96df6e014eaf49570c6
shashank <shashankskb18700@gmail.com>
2021-04-10 14:50:40 +0530

    added redux,and implemented userReducer and userAction in header and App component

--- src/App.js
+++ src/App.js
@@ -13,32 +13,24 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component{
-  constructor() {
-    super();
-    this.state = {
-     currentUser:null 
-   }
-  }
+ 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
+         
               id: snapShot.id,
               ...snapShot.data()
               
-            }
-          },
-         )   
-        }
-      )
-
+            })
+        })
       }
-      this.setState({currentUser:userAuth})
+      setCurrentUser({currentUser:userAuth})
    })
   }
 
@@ -59,9 +51,11 @@ class App extends React.Component{
       </div>
     );
   }
-  
-  
-  
+ 
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchToProps)(App);
